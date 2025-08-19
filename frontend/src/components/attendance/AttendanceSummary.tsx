9855319b9201ba28ebdb1632933ba9029b150aa9
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2025-08-19 10:56:19 +0300

    loading whn fetching attendance

--- frontend/src/components/attendance/AttendanceSummary.tsx
+++ frontend/src/components/attendance/AttendanceSummary.tsx
@@ -210,74 +210,83 @@ const AttendanceSummary: React.FC = () => {
       )}
 
       {/* Report Section */}
-      {!noAttendance && shiftAttendance && (
-        <Card title="ملخص الحضور" className="shadow-md">
-          {/* total scurity guards */}
-          <div
-            className="bg-blue-50 border border-blue-200 rounded-md p-4 flex items-center 
+      {fetchingAttendance ? (
+        <Loading />
+      ) : (
+        !noAttendance &&
+        shiftAttendance && (
+          <Card title="ملخص الحضور" className="shadow-md">
+            {/* total scurity guards */}
+            <div
+              className="bg-blue-50 border border-blue-200 rounded-md p-4 flex items-center 
           gap-4 text-blue-800 shadow-sm w-full sm:max-w-xs mb-10 mx-auto"
-          >
-            <TeamOutlined className="text-2xl text-blue-600" />
-            <Statistic
-              title={
-                <span className="text-sm text-blue-800">إجمالي رجال الأمن</span>
-              }
-              value={shiftAttendance.records.length}
-              valueStyle={{ color: "#1e40af", fontWeight: 600 }}
-            />
-          </div>
-
-          {/* shift attendanc data */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {/* Pie Chart */}
-            <div>
-              <Title level={5}>نسبة الحضور</Title>
-              <PieChart width={300} height={300}>
-                <Pie
-                  data={shiftAttendance?.stats || []}
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={100}
-                  dataKey="value"
-                  label
-                >
-                  {shiftAttendance?.stats?.map((entry: any, index: number) => (
-                    <Cell key={`cell-${index}`} fill={entry.color} />
-                  ))}
-                </Pie>
-                <Tooltip />
-                <Legend />
-              </PieChart>
-            </div>
-
-            {/* Table */}
-            <div>
-              <Title level={5}>تفاصيل الحضور</Title>
-              <Table
-                columns={columns}
-                dataSource={shiftAttendance?.records}
-                rowKey={"id"}
-                pagination={false}
-                className="calypso-header"
-                scroll={{ x: "max-content" }}
+            >
+              <TeamOutlined className="text-2xl text-blue-600" />
+              <Statistic
+                title={
+                  <span className="text-sm text-blue-800">
+                    إجمالي رجال الأمن
+                  </span>
+                }
+                value={shiftAttendance.records.length}
+                valueStyle={{ color: "#1e40af", fontWeight: 600 }}
               />
             </div>
-          </div>
 
-          {/* timestamp */}
-          <div
-            className="bg-green-50 border border-green-200 rounded-md p-4 
+            {/* shift attendanc data */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {/* Pie Chart */}
+              <div>
+                <Title level={5}>نسبة الحضور</Title>
+                <PieChart width={300} height={300}>
+                  <Pie
+                    data={shiftAttendance?.stats || []}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    dataKey="value"
+                    label
+                  >
+                    {shiftAttendance?.stats?.map(
+                      (entry: any, index: number) => (
+                        <Cell key={`cell-${index}`} fill={entry.color} />
+                      )
+                    )}
+                  </Pie>
+                  <Tooltip />
+                  <Legend />
+                </PieChart>
+              </div>
+
+              {/* Table */}
+              <div>
+                <Title level={5}>تفاصيل الحضور</Title>
+                <Table
+                  columns={columns}
+                  dataSource={shiftAttendance?.records}
+                  rowKey={"id"}
+                  pagination={false}
+                  className="calypso-header"
+                  scroll={{ x: "max-content" }}
+                />
+              </div>
+            </div>
+
+            {/* timestamp */}
+            <div
+              className="bg-green-50 border border-green-200 rounded-md p-4 
           flex items-center gap-3 text-green-800 shadow-sm mt-10"
-          >
-            <CheckCircleOutlined className="text-xl" />
-            <span className="text-sm sm:text-base font-medium">
-              تم تسجيل الحضور في
-              <span className="mx-1 font-bold">
-                {shiftAttendance.created_at}
+            >
+              <CheckCircleOutlined className="text-xl" />
+              <span className="text-sm sm:text-base font-medium">
+                تم تسجيل الحضور في
+                <span className="mx-1 font-bold">
+                  {shiftAttendance.created_at}
+                </span>
               </span>
-            </span>
-          </div>
-        </Card>
+            </div>
+          </Card>
+        )
       )}
     </div>
   );
